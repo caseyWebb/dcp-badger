9d315b69f5c22f327867aae683a0d9d3d021f327
Casey Webb <notcaseywebb@gmail.com>
2018-11-26 23:01:59 -0600

    refactor: use ejs template

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,39 @@
 import * as fs from 'fs-extra'
 import * as path from 'path'
+import _ from 'lodash'
 import Koa from 'koa'
 
 const host = 'localhost'
 const port = 3000
 
-const app = new Koa()
+type Template = (data: TemplateData) => string
 
-app.use(async (ctx) => {
-  const image = await fs.createReadStream(path.resolve(__dirname, './svg.svg'))
-  ctx.body = image
-  ctx.status = 200
-  ctx.type = 'image/svg+xml'
-})
+type TemplateData = {}
+;(async function main() {
+  const app = new Koa()
+  const template = await createTemplate()
 
-app
-  .listen(port, host)
-  .on('listening', () => console.log(`Server started on ${host}:${port}`))
+  app.use(createRequestHandler(template))
+
+  const server = app.listen(port, host)
+
+  server.on('listening', () => console.log(`Server started on ${host}:${port}`))
+
+  process.once('SIGUSR2', () => server.close())
+})()
+
+function createRequestHandler(template: Template) {
+  return async function requestHandler(ctx: Koa.Context) {
+    ctx.body = template({})
+    ctx.status = 200
+    ctx.type = 'image/svg+xml'
+  }
+}
+
+async function createTemplate(): Promise<Template> {
+  const templateString = await fs.readFile(
+    path.resolve(__dirname, './image.ejs'),
+    'utf-8'
+  )
+  return _.template(templateString)
+}

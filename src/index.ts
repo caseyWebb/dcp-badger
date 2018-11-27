3784c6e95cc828a788df60e37d6f5a7f5cbae44b
Casey Webb <notcaseywebb@gmail.com>
2018-11-27 14:03:12 -0600

    Fill in completed days using git repo data

--- src/index.ts
+++ src/index.ts
@@ -3,8 +3,19 @@ import * as path from 'path'
 import axios from 'axios'
 import Koa from 'koa'
 import { template as ejs } from 'lodash'
-import { filter, flow, map, uniq } from 'lodash/fp'
-import { addWeeks, getDate, getMonth, setDay, subMonths, format } from 'date-fns'
+import { filter, flow, map, padCharsStart, uniq } from 'lodash/fp'
+import {
+  addWeeks,
+  getDate as getDayOfMonth,
+  getMonth,
+  setDate as setDayOfMonth,
+  setDay,
+  setMonth,
+  setYear,
+  subMonths,
+  subWeeks,
+  format as formatDate
+} from 'date-fns'
 
 const host = 'localhost'
 const port = 3000
@@ -15,11 +26,7 @@ const extensions = {
   typescript: 'ts'
 }
 
-type Template = (data: TemplateData) => string
-
-type TemplateData = {
-
-}
+type Template = (scope: any) => string
 
 /**
  * Main app entry point. Creates and starts the server.
@@ -40,26 +47,40 @@ async function main() {
 function createRequestHandler(template: Template) {
   return async function requestHandler(ctx: Koa.Context) {
     const completedDays = await getCompletedDays()
-    console.log(completedDays)
     ctx.body = template({
-      getFill,
+      getDate,
+      getFill: (d: Date) => getFill(completedDays, d),
       getMonthLabel,
-      getMonthX
+      getMonthX,
+      formatDate
     })
     ctx.status = 200
     ctx.type = 'image/svg+xml'
   }
 }
 
-function getFill(w: number, d: number) {
-  return '#ebedf0'
+function getDate(w: number, dw: number) {
+  const weeksAgo = 53 - w
+  let date = new Date()
+  date = subWeeks(date, weeksAgo)
+  date = setDay(date, dw)
+  return date
+}
+
+function getFill(completedDays: Set<string>, date: Date) {
+  const now = new Date()
+  if (date > now) {
+    return 'rgba(0,0,0,0)'
+  }
+  const isComplete = completedDays.has(formatDate(date, 'yyyy.MM.dd'))
+  return isComplete ? '#7bc96f' : '#ebedf0'
 }
 
 function getMonthLabel(m: number) {
   const monthsAgo = 12 - m
   const now = new Date()
   const then = subMonths(now, monthsAgo)
-  return format(then, 'MMM')
+  return formatDate(then, 'MMM')
 }
 
 function getMonthX(m: number) {
@@ -68,7 +89,7 @@ function getMonthX(m: number) {
   const targetMonth = getMonth(subMonths(now, monthsAgo))
   let then = setDay(subMonths(now, 12), 0)
   let column = 0
-  while (getMonth(then) !== targetMonth || getDate(then) > 7) {
+  while (getMonth(then) !== targetMonth || getDayOfMonth(then) > 7) {
     column++
     then = addWeeks(then, 1)
   }
@@ -85,7 +106,7 @@ async function createTemplate(): Promise<Template> {
 
 /**
  * Returns an array of normalized date strings.
- * 
+ *
  * .indexOf/.includes in V8 is wicked fast, so this really isn't worth trying to do any
  * crazy optimizations (for now)
  */
@@ -98,17 +119,21 @@ async function getCompletedDays() {
   const url = `https://api.github.com/repos/${owner}/${repo}/git/trees/${ref}?recursive=1`
   const res = await axios.get(url)
   const regex = new RegExp(
-    `(?<year>\\d+)/(?<month>\\d+)/(?<day>\\d+)\\.${ext}`,
+    `(?<year>\\d{4})/(?<month>\\d+)/(?<day>\\d+)\\.${ext}`,
     'u'
   )
-  return flow(
-    filter(({ type }: any) => type === 'blob'),
-    map(({ path }: any) => regex.exec(path)),
-    filter((f: RegExpExecArray) => f !== null),
-    map((f: RegExpExecArray) => f.groups),
-    map((f: any) => `${f.year}.${f.month}.${f.day}`),
-    uniq,
-  )(res.data.tree)
+  const zeroPad = padCharsStart('0')
+  const zeroPad2 = zeroPad(2)
+  return new Set(
+    flow(
+      filter(({ type }: any) => type === 'blob'),
+      map(({ path }: any) => regex.exec(path)),
+      filter((f: RegExpExecArray) => f !== null),
+      map((f: RegExpExecArray) => f.groups),
+      map((f: any) => `${f.year}.${zeroPad2(f.month)}.${zeroPad2(f.day)}`),
+      uniq
+    )(res.data.tree)
+  )
 }
 
 main()
